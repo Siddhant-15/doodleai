2f7bd542d9c7d5c672a661217209017285830e7d
Siddhant-15 <siddhantjanbandhu18@gmail.com>
2025-01-07 18:34:25 +0530

    updated root->page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,10 @@ import { getAllImages } from "@/lib/actions/image.actions"
 import Image from "next/image"
 import Link from "next/link"
 
+interface SearchParamProps {
+  searchParams: Record<string, string | string[] | undefined>;
+}
+
 const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
