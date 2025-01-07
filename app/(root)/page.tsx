c07201378f7a36c8c3f7dffd88304de79d099195
Siddhant-15 <siddhantjanbandhu18@gmail.com>
2025-01-07 19:09:03 +0530

    updated searchparams

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,23 @@
-import { Collection } from "@/components/shared/Collection"
-import { navLinks } from "@/constants"
-import { getAllImages } from "@/lib/actions/image.actions"
-import Image from "next/image"
-import Link from "next/link"
+import { Collection } from "@/components/shared/Collection";
+import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.actions";
+import Image from "next/image";
+import Link from "next/link";
 
-
-const Home = async ({ searchParams }: { searchParams: { page: string | undefined; query: string | undefined } }) => {
+const Home = async ({ searchParams }: { searchParams: Record<string, string | undefined> }) => {
+  // Destructure and await the `searchParams` properly
   const page = Number(searchParams?.page) || 1;
-  const searchQuery = (searchParams?.query as string) || '';
+  const searchQuery = searchParams?.query || '';
 
-  const images = await getAllImages({ page, searchQuery})
+  const images = await getAllImages({ page, searchQuery });
 
   return (
     <>
       <section className="home">
-        <h1 className="home-heading">
-          Unleash Your Creative Vision with Imaginify
-        </h1>
+        <h1 className="home-heading">Unleash Your Creative Vision with Imaginify</h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
-            <Link
-              key={link.route}
-              href={link.route}
-              className="flex-center flex-col gap-2"
-            >
+            <Link key={link.route} href={link.route} className="flex-center flex-col gap-2">
               <li className="flex-center w-fit rounded-full bg-white p-4">
                 <Image src={link.icon} alt="image" width={24} height={24} />
               </li>
@@ -34,7 +28,7 @@ const Home = async ({ searchParams }: { searchParams: { page: string | undefined
       </section>
 
       <section className="sm:mt-12">
-        <Collection 
+        <Collection
           hasSearch={true}
           images={images?.data}
           totalPages={images?.totalPage}
@@ -42,7 +36,7 @@ const Home = async ({ searchParams }: { searchParams: { page: string | undefined
         />
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
